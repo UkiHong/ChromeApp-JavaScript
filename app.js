a6fe1a848ead94a21dd2da3a228b846a12ce5fd2
UkiHong <suhong9507@gmail.com>
2023-08-22 23:12:37 +0100

    2

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const a = 5;
-const b = 2;
-const myName = "uki";
+const calculator = {
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
+  },
+  square: function (a, b) {
+    return a ** b;
+  },
+};
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+const plusResult = calculator.plus(1, 2);
+const minusResult = calculator.minus(plusResult, 4);
+const timesResult = calculator.times(minusResult, plusResult);
+const squareResult = calculator.square(timesResult, 4);
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(squareResult);
