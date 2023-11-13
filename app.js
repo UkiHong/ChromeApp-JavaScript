fa7b44f74c8c28fae30beb0c1c107ed3b8927d99
UkiHong <suhong9507@gmail.com>
2023-11-13 02:49:39 +0000

    js

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+function handleTitleClick() {
+  h1.style.color = "blue";
+}
 
-title.style.color = "red";
+function handleMouseenter() {
+  h1.innerText = "Mouse is here";
+}
+
+function handleMouseleave() {
+  h1.innerText = "Mouse left";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copied");
+}
+
+function handleWindowOffline() {
+  alert("No WIFI");
+}
+
+function handleWindowOnline() {
+  alert("WIFI connected");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseenter);
+h1.addEventListener("mouseleave", handleMouseleave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
