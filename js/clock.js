a081ce2ac57d1f3eb2cd80171bc33cd5f7a816b6
UkiHong <suhong9507@gmail.com>
2023-11-17 02:33:12 +0000

    background img

--- js/clock.js
+++ js/clock.js
@@ -7,6 +7,8 @@ function getClock() {
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+//padStart(총 스트링 개수, 추가할 스트링 문자)
+//String 으로 표현하는 법은 String(괄호 안에 씌워주면 끝)
 
 getClock();
 setInterval(getClock, 1000);
