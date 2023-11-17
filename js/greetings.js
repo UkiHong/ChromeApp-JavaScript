e017454f0c0d62de402d11991eb9cd3edf922649
UkiHong <suhong9507@gmail.com>
2023-11-17 01:23:43 +0000

    clock

---
+++ js/greetings.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+// key value setting
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+//username submit function
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+//greetings messages
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+//checking username in localStorage and submitting form
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+  //show the form
+} else {
+  paintGreetings(savedUsername);
+  // show the greeting
+}
