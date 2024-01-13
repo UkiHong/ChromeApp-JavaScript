5428f8be18a1288d5aa73d2cb702a91a95b4a105
UkiHong <suhong9507@gmail.com>
2024-01-13 21:16:55 +0000

    weather finish and css

--- js/background.js
+++ js/background.js
@@ -4,7 +4,13 @@ const chosenImage = images[[Math.floor(Math.random() * images.length)]];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+//bgImage.src = `img/${chosenImage}`;
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundPosition = "top";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundAttachment = "fixed";
 
 //background images 깔끔하게 넣는법
 //bgImage.src = `img/${chosenImage}`; 아 코드 지우고 아래 입력
--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "a915af1fe8b16786bf05f62e36c3c7e6";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("you live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      const temp = Math.floor(data.main.temp);
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${temp}`;
+    });
 }
 
 function onGeoError() {
